c0693cb9918ed6761bdbf7abb8807aca54e27cc1
Saqlain Tahir <87969114+saqlain-tahir-qbatch@users.noreply.github.com>
2021-10-21 20:00:46 +0500

    Revert "extra code removed"

--- Code/todo_app/src/components/SignUp.js
+++ Code/todo_app/src/components/SignUp.js
@@ -1,35 +1,32 @@
-import React, { useState } from "react";
+import React, {useState}from 'react'
+import PropTypes from 'prop-types'
 
-const SignUp = () => {
-  const [state, setState] = useState({ name: "", email: "", password: "" });
-  const handleSubmit = () => {};
-  return (
-    <div>
-      <p> This is SignUp Page </p>
-      <form>
-        <lable> Name </lable>
-        <input
-          type="text"
-          value={state.name}
-          onChange={(event) => setState({ name: event.target.value })}
-        />
-        <lable> Email </lable>
-        <input
-          type="text"
-          value={state.email}
-          onChange={(event) => setState({ email: event.target.value })}
-        />
-        <lable> Password </lable>
-        <input
-          type="text"
-          value={state.password}
-          onChange={(event) => setState({ password: event.target.value })}
-        />
+const SignUp = (props)=> {
+     const [state, setState] =useState({  name:"" , email: "" , password: ""})
+     const handleSubmit =() =>{
+         
+     }
+    return (
+        <div>
+             <p> This is SignUp Page </p> 
+             <form>
+                <lable> Name </lable>
+                <input type="text" value={state.name} onChange= {event => setState({name:event.target.value}) } />
+                <lable> Email </lable>
+                <input type="text" value={state.email} onChange= {event => setState({email:event.target.value})} />
+                <lable> Password </lable>
+                <input type="text" value={state.password} onChange= {event => setState({password:event.target.value})} />
 
-        <button type="submit" onClick={handleSubmit}></button>
-      </form>
-    </div>
-  );
-};
+                <button type='submit' onClick={handleSubmit}></button>
+            </form>
+        </div>
+    )
+}
 
-export default SignUp;
+SignUp.propTypes = {
+
+}
+
+export default SignUp
+
+
